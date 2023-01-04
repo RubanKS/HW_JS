daa6b903f8914160ba7e1a5b30a817ae8c70562c
Konstantin <kosru86@gmail.com>
2023-01-05 00:17:20 +0300

    HW 1

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let tCels = 1;
+let tFar = tCels * (9 / 5) + 32;
+console.log(`Температура в Цельсии = ${tCels}
+Температура в Фарингейта = ${tFar}`)
+
+let name = 'Konstantin';
+let admin = name;
+console.log(`${admin}`)
