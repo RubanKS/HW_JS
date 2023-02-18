a95d0d2452eb33b7103466f4b99ee132b9c05f69
Konstantin <kosru86@gmail.com>
2023-02-18 22:40:36 +0300

    HW 10

---
+++ HW_10/data.js
@@ -0,0 +1,23 @@
+// http://numbersapi.com/random/year?json
+const dataInfo = `
+[
+	{
+		"text": "544 is the year that Jacob Baradaeus consecrates Sergius of Tella as Patriarch of Antioch.",
+		"number": 544,
+		"found": true,
+		"type": "year"
+	},
+	{
+		"text": "156 is the year that the heresiarch Montanus first appears in Ardaban (Mysia).",
+		"number": 156,
+		"found": true,
+		"type": "year"
+	},
+	{
+  "text": "96 BC is the year that Start of the Taishi era in the Han Dynasty.",
+  "number": -96,
+  "found": true,
+  "type": "year"
+}
+]
+`
